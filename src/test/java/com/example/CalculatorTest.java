8932d66998ed7282f1243139393f75551f873593
rpenmetsa <ps.raju@yahoo.com>
2025-10-28 18:19:45 +0200

    update time

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -23,7 +23,7 @@ class CalculatorTest {
     // Runs once before all tests in this class
     @BeforeAll
     static void initAll() {
-        System.out.println("=== Starting Calculator Test Suite ===");
+        System.out.println("=== Starting Calculator Test Suite Update time===");
         testCounter = 0;
     }
 
